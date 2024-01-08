e37ce1a0390734c514dc829aa955ef5783406fbb
bygabymora <bygabymora@gmail.com>
2024-01-08 00:01:30 -0500

    Add categorySpent state and update category spending amounts

--- pages/admin/consumos.js
+++ pages/admin/consumos.js
@@ -8,6 +8,7 @@ export default function Consumos() {
   const [consumos, setConsumos] = useState([]);
   const [paymentTypeSums, setPaymentTypeSums] = useState([]);
   const [fecha, setFecha] = useState(new Date());
+  const [categorySpent, setCategorySpent] = useState({});
 
   const monthPlusOne = fecha.getMonth();
   const yearPlusOne = fecha.getFullYear();
@@ -39,13 +40,50 @@ export default function Consumos() {
       const typeOfPurchaseResult = await axios(
         `/api/admin/products?action=aggregateTypeOfPurchase&month=${month}&year=${year}`
       );
+
       setConsumos(typeOfPurchaseResult.data.consumos || []);
 
       const paymentTypeResult = await axios(
         `/api/admin/products?action=aggregatePaymentType&month=${month}&year=${year}`
       );
+
       setPaymentTypeSums(paymentTypeResult.data);
+
+      const initialCategorySpent = {
+        'Comida y aseo': { spent: 0, maxAmount: 3000000 },
+        'Extras Casa': { spent: 0, maxAmount: 1000000 },
+        Medicinas: { spent: 0, maxAmount: 500000 },
+        'Mesada Martina': { spent: 0, maxAmount: 100000 },
+        'Mesada Rafaela': { spent: 0, maxAmount: 100000 },
+        'Clases Pollos': { spent: 0, maxAmount: 370000 },
+        Gasolina: { spent: 0, maxAmount: 880000 },
+        Mantenimiento: { spent: 0, maxAmount: 200000 },
+        Lavado: { spent: 0, maxAmount: 80000 },
+        Parqueadero: { spent: 0, maxAmount: 50000 },
+        Peajes: { spent: 0, maxAmount: 100000 },
+        Papeles: { spent: 0, maxAmount: 100000 },
+        'Ocio General': { spent: 0, maxAmount: 500000 },
+        Viajes: { spent: 0, maxAmount: 400000 },
+        Cumpleaños: { spent: 0, maxAmount: 400000 },
+        'Comidas afuera': { spent: 0, maxAmount: 800000 },
+        'Ropa Pollos': { spent: 0, maxAmount: 250000 },
+        'Ropa Papás': { spent: 0, maxAmount: 250000 },
+        'Comida Perros': { spent: 0, maxAmount: 300000 },
+        'Guardería Perros': { spent: 0, maxAmount: 200000 },
+        'Medicina Perros': { spent: 0, maxAmount: 100000 },
+        // Add any other categories you need here in a similar fashion
+      };
+
+      // Update spent amounts for each category based on fetched data
+      typeOfPurchaseResult.data.consumos.forEach((item) => {
+        if (initialCategorySpent[item._id]) {
+          initialCategorySpent[item._id].spent = item.totalValue;
+        }
+      });
+
+      setCategorySpent(initialCategorySpent);
     };
+
     fetchData();
   }, [fecha]);
 
@@ -144,11 +182,27 @@ export default function Consumos() {
               Presupuesto disponible
             </h1>
             <div>
-              <div>
-                <h1 className="text-2xl font-bold border-b border-gray-300">
-                  Comiday aseo
-                </h1>
-                <div></div>
+              <div className="grid grid-cols-3 mb-5">
+                {Object.entries(categorySpent).map(([category, budget]) => (
+                  <div key={category}>
+                    {categorySpent[category].maxAmount > 0 && (
+                      <div key={category}>
+                        <h2 className="text-xl font-bold">{category}</h2>
+                        <p>
+                          Gastado: ${formatNumberWithDots(budget.spent)}
+                          <br />
+                          Presupuesto Máximo: $
+                          {formatNumberWithDots(budget.maxAmount)}
+                          <br />
+                          Restante: $
+                          {formatNumberWithDots(
+                            budget.maxAmount - budget.spent
+                          )}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -157,6 +211,7 @@ export default function Consumos() {
             <h1 className="text-2xl font-bold border-b border-gray-300">
               Por Categoria
             </h1>
+
             <div className="grid grid-cols-1 gap-1 md:grid-cols-3">
               {aggregatedSums.map(({ mainCategory, totalSum }) => (
                 <div key={mainCategory} className="card p-2 text-center">
